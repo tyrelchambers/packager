02c0ceb47069e9abeaee1a029b6527121bf3d246
Tyrel Chambers <tychambers3@gmail.com>
2021-06-19 20:40:53 -0400

    .

--- components/NPMSearch/NPMSearch.jsx
+++ components/NPMSearch/NPMSearch.jsx
@@ -13,7 +13,7 @@ const NPMSearch = ({ dispatch }) => {
   const [deps, setDeps] = useState([]);
 
   const searchNpm = async (q) => {
-    return await axios.get(`${process.env.API}/api/npmSearch`, {
+    return await axios.get(`/api/npmSearch`, {
       params: { q },
     });
   };
--- next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  env: {
-    API: "http://localhost:3000",
-  },
-};
